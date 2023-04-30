07a5e8a266e22b9f77859296e6c266a8958063dd
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-04-30 22:32:54 +0900

    Create 문자와 문자열.java

---
+++ 5주차 Java/1차원 배열/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String[] str = sc.next().split("");
+        int i = sc.nextInt();
+        
+        System.out.println(str[i-1]);
+        
+        
+	}
+}
