9c590b8c1765cc4d7b37c2e561b36d7fd6d47ea5
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-04-30 16:03:28 +0900

    Update 시험 성적.java

--- 5주차 Java/조건문/9498. 시험 성적/시험 성적.java
+++ 5주차 Java/조건문/9498. 시험 성적/시험 성적.java
@@ -7,22 +7,22 @@ class Main {
 		int score = sc.nextInt();
         
 		switch(score/10){
-            case 10:
-            case 9:
-                System.out.println("A");
-                break;
-            case 8:
-                System.out.println("B");
-                break;
-            case 7:
-                System.out.println("C");
-                break;
-            case 6:
-                System.out.println("D");
-                break;
-            default:
-                System.out.println("F");
-                break;
-        }
+		    case 10:
+		    case 9:
+			System.out.println("A");
+			break;
+		    case 8:
+			System.out.println("B");
+			break;
+		    case 7:
+			System.out.println("C");
+			break;
+		    case 6:
+			System.out.println("D");
+			break;
+		    default:
+			System.out.println("F");
+			break;
+		}
 	}
 }   
